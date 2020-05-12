2a83ee052061e498cc914141210e5634aedefeb9
Andriy <andriy.shakh@epitech.eu>
2020-05-12 23:40:45 +0200

    Username [in progress]

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const http = require('http');
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/welcome.html');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post('/chat', (req, res) => {
+  console.log(req.body.username)
+  res.sendFile(__dirname + '/chat.html');
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
